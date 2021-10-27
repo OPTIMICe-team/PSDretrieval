e841baf43b540dda94ba2f5552c5fffe6ef51907
Markus Karrer <markus.karrer@uni-koeln.de>
2021-10-27 09:16:11 +0200

    start basic architecture

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+long_description = """PSDretrieval retrieves the particle size distribution from multi-frequency radar Doppler spectra.
+"""
+
+setup(name='PSDretrieval',
+      description='PSD retrieval',
+      author='Markus Karrer',
+      author_email='karrer.markus@web.de',
+      url='https://github.com/markuskarrer/PSDretrieval',
+      packages=['PSDretrieval'],
+      long_description = long_description,
+      license = 'GPL'
+     )
