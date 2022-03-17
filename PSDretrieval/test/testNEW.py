00f65f785f5d211e4669e997c42fa98c69550f4e
Markus Karrer <markus.karrer@uni-koeln.de>
2022-03-17 10:02:19 +0100

    update test.NEW script

--- PSDretrieval/test/testNEW.py
+++ PSDretrieval/test/testNEW.py
@@ -37,16 +37,16 @@ else:
     PeaksWindow  = pR.loadPeaks(loadSample=False,dataPath="/data/obs/campaigns/tripex-pol/spectralPeaks/",createSample=True,date=date,time=time,tRange=1,hRange=180,hcenter=hRange)
 
 #get vertical wind information from the Spectral data
-#SpecWindow = pR.addVerticalWindToSpecWindow(SpecWindow,PeaksWindow)
-SpecWindow = pR.addVerticalWindToSpecWindow(SpecWindow,None,addManually=True,manualW=manualW)
+SpecWindow = pR.addVerticalWindToSpecWindow(SpecWindow,PeaksWindow)
+#SpecWindow = pR.addVerticalWindToSpecWindow(SpecWindow,None,addManually=True,manualW=manualW)
 SpecSingle  = pR.selectSingleTimeHeight(SpecWindow)
 SpecSingleWshifted  = pR.shiftSpectra(SpecSingle)
 
 #plot Spectra and sDWR
 fig,ax = plt.subplots(nrows=1,ncols=1)
-#fig2,axes2 = plt.subplots(nrows=1,ncols=2)
+fig2,axes2 = plt.subplots(nrows=1,ncols=2)
 __ = pl.plotObsSpectra(SpecSingleWshifted,ax)
-#__ = pl.plotSDWRvsDVobs(SpecSingle,axes2)
+__ = pl.plotSDWRvsDVobs(SpecWindow,axes2)
 
 #apply stricter noise threshold (needed for particleType selection of snowScatt models)
 SpecWindow = pR.cutLowZe(SpecWindow,zeThreshold=-20)
@@ -61,15 +61,13 @@ SpecWindow = pR.cutLowZe(SpecWindow,zeThreshold=-20)
 
 #find best fitting particle type
 #[bestPartType,orderedListPartType] = rU.findBestFittingPartType(ParticleTypesList,SpecWindow,verbose=True,whichDWRsToUse="DWR_Ka_W")
-bestPartType = "vonTerzi_needle"
+bestPartType = "vonTerzi_column"
 
 #plot sDWR vs DV for best fitting particle type
 ZxModel, ZkModel, ZwModel, Dmax, K2, velModel = sc.model3fOne(bestPartType)
-DWRxk = ZxModel - ZkModel; DWRkw = ZkModel - ZwModel
-DWRxkModelUnAmb,ax = sc.getUnambigousDWRdmax(Dmax,DWRxk)
-DWRkwModelUnAmb,ax = sc.getUnambigousDWRdmax(Dmax,DWRkw)
-#axes2 = pl.plotSDWRvsDVmodel(velModel,DWRxkModelUnAmb,DWRkwModelUnAmb,axes2,bestPartType)
-#axes2[0].legend()
+DWRxkModel = ZxModel - ZkModel; DWRkwModel = ZkModel - ZwModel
+axes2 = pl.plotSDWRvsDVmodel(velModel,DWRxkModel,DWRkwModel,axes2,bestPartType)
+axes2[0].legend()
 
 #display the single particle reflectivity
 #fig3,ax = plt.subplots(nrows=1,ncols=1)
